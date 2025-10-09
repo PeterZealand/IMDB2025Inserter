627ba741a29e7a9b65dc30c7924666150f2d1925
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2025-10-09 13:20:09 +0200

    Created Principals

--- IMDB2025Inserter/Principals.cs
+++ IMDB2025Inserter/Principals.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IMDB2025Inserter
 {
-    internal class Principals
+    public class Principals
     {
+
+        public int TitleId { get; set; }
+
+        public int Ordering { get; set; }
+
+        public int NameId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Job { get; set; }
+
+        public string Characters { get; set; }
+ 
+        public Principals() { }
+
+        public string ToSQL()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"INSERT INTO Principals (TitleId, Ordering, NameId, Category, Job, Characters) VALUES (");
+            sb.Append($"{TitleId}, ");
+            sb.Append($"{Ordering}, ");
+            sb.Append($"{NameId}, ");
+            sb.Append($"'{Category.Replace("'", "''")}', ");
+            sb.Append(!string.IsNullOrEmpty(Job) ? $"'{Job.Replace("'", "''")}', " : "NULL, ");
+            sb.Append(!string.IsNullOrEmpty(Characters) ? $"'{Characters.Replace("'", "''")}'" : "NULL");
+            sb.Append(");");
+            return sb.ToString();
+        }
     }
 }
