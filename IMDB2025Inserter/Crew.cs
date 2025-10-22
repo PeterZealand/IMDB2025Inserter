de0d7d95e4d17d73922b60b12e0bbee87fe9c7ef
Jais Andersen <jais.h.sorensen@gmail.com>
2025-10-22 13:49:44 +0200

    Crew Model klasse

---
+++ IMDB2025Inserter/Crew.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB2025Inserter
+{
+    public class Crew
+    {
+        public int Id { get; set; }
+        public List<string> Directors { get; set; }
+        public List<string> Writers { get; set; }
+        public Crew()
+        {
+            Directors = new List<string>();
+            Writers = new List<string>();
+        }
+        public string ToSQL()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"INSERT INTO Crews (Id) VALUES (");
+            sb.Append($"{Id}");
+            sb.Append(");");
+            
+            return sb.ToString();
+        }
+    }
+}
